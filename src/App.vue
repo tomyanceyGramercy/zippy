c29453454284f34172d498c704bd30851129ae1a
tomyanceyGramercy <65623232+tomyanceyGramercy@users.noreply.github.com>
2024-10-26 10:39:27 -0700

    Initial commit with Vite template

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to zippy</h1>
+    <HelloWorld msg="You did it!" />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h3>
+      {{ msg }}
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h3 {
+  font-size: 1.2rem;
+  color: #213547;
+}
+</style>
